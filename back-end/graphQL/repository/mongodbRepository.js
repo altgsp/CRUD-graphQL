43ad5b29f6b6e031da8f1f37fd37a16d8a613e49
Geisa Santos <geisa.santos.dir@gmail.com>
2022-04-25 21:27:56 -0300

    cria e conecta repositório banco de dados mongodb

---
+++ back-end/graphQL/repository/mongodbRepository.js
@@ -0,0 +1,29 @@
+// conectando ao banco de dados e expondo operações CRUD nas entradas do banco.
+
+class MongodbRepo {
+    constructor(collectionName) {
+        this.collection = getDB().collection(collectionName);
+    }
+
+    getAll() {
+        return new Promise((resolve, reject) => {
+            this.collection.find({}).toArray((err, data) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(data);
+            });
+        });
+    }
+
+    getById(_id) {
+        return new Promise((resolve, reject) => {
+            this.collection.findOne({ _id: ObjectId(_id) }, (err, data) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(data);
+            });
+        });
+    }
+}
