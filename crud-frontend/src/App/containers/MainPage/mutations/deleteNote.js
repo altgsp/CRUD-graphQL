c00b0900e9beaa024000f3de9e97bdd6cccc200a
Geisa Santos <geisa.santos.dir@gmail.com>
2022-05-01 20:20:54 -0300

    atualiza CRUD e implementa funções de resposta

---
+++ crud-frontend/src/App/containers/MainPage/mutations/deleteNote.js
@@ -0,0 +1,31 @@
+import { commitMutation, graphql } from 'react-relay';
+import environment from '../../../../environment';
+
+const mutation = graphql`
+    mutation deleteNoteMutation($_id: ID) {
+        deleteNote(_id $_id)
+    }
+`;
+
+function deleteNoteMutation(_id) {
+    const variables = {
+        _id
+    };
+    commitMutation(environment, {
+        mutation,
+        variables,
+        onCompleted: (response, errors) => {
+            console.log('Resposta recebida do servidor.');
+        },
+        updater: store => {
+            const root = store.getRoot();
+            const notes = root.getLinkedRecords('notes');
+            const newNotes = notes.filter(v => v.getValue('_id') !== _id);
+
+            root.setLinkedRecords(newNotes, 'notes');
+        },
+        onError: err => console.error(err)
+    });
+}
+
+export default deleteNoteMutation;
